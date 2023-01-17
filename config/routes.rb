132de19f67096374f4528859aa9e02a3b8ab623c
kelsonbatista <kelsonbatista@gmail.com>
2023-01-17 10:13:31 -0300

    feat(vehicle): added index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: "home#index"
   resources :modes, only: [:index, :show, :new, :create]
+  resources :vehicles, only: [:index]
 end
