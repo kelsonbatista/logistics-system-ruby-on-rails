820d9285b08e4a8406cff771072a15d91288b581
kelsonbatista <kelsonbatista@gmail.com>
2023-01-18 19:17:53 -0300

    feat(price): added created method

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -2,4 +2,27 @@ class PricesController < ApplicationController
   def index
     @prices = Price.all
   end
+
+  def new
+    @price = Price.new
+    @modes = Mode.all
+  end
+
+  def create
+    @price = Price.new(price_params)
+    if @price.save
+      flash[:notice] = 'Preço cadastrado com sucesso'
+      return redirect_to prices_path
+    end
+    @modes = Mode.all
+    flash.now[:alert] = 'Erro ao cadastrar preço'
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def price_params
+    params.require(:price).permit(:min_weight, :max_weight, :price_per_km, :mode_id)
+  end
+
 end
