b1c9c15016dc1a62ae0ac10674fa23175b3d8433
kelsonbatista <kelsonbatista@gmail.com>
2023-01-19 01:51:30 -0300

    feat(deadline): added edit method

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -1,5 +1,47 @@
 class DeadlinesController < ApplicationController
+  before_action :find_deadline, only: [:edit, :update, :destroy]
+
   def index
     @deadlines = Deadline.all
+  end
+
+  def new
+    @deadline = Deadline.new
+    @modes = Mode.all.order(:name)
+  end
+
+  def create
+    @deadline = Deadline.new(deadline_params)
+    if @deadline.save
+      flash[:notice] = 'Prazo cadastrado com sucesso'
+      return redirect_to deadlines_path
+    end
+    @modes = Mode.all.order(:name)
+    flash.now[:alert] = 'Erro ao cadastrar prazo'
+    render :new, status: :unprocessable_entity
+  end
+
+  def edit
+    @modes = Mode.all.order(:name)
+  end
+
+  def update
+    if @deadline.update(deadline_params)
+      flash[:notice] = "Prazo atualizado com sucesso"
+      return redirect_to deadlines_path
+    end
+    @modes = Mode.all.order(:name)
+    flash.now[:alert] = "Erro ao atualizar o prazo"
+    render :edit, status: :unprocessable_entity
+  end
+
+  private
+
+  def deadline_params
+    params.require(:deadline).permit(:min_distance, :max_distance, :deadline, :mode_id)
+  end
+
+  def find_deadline
+    @deadline = Deadline.find(params[:id])
   end
 end
