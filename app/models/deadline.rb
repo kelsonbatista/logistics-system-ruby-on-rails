03138be04a7ea7888563389427773cd92a7e8dc7
kelsonbatista <kelsonbatista@gmail.com>
2023-01-19 02:07:05 -0300

    feat(deadline): added create method

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -1,3 +1,4 @@
 class Deadline < ApplicationRecord
   belongs_to :mode
+  validates :min_distance, :max_distance, :deadline, :mode_id, presence: true
 end
--- app/models/mode.rb
+++ app/models/mode.rb
@@ -1,6 +1,7 @@
 class Mode < ApplicationRecord
   has_many :vehicles
   has_many :prices
+  has_many :deadlines
   validates :name, :min_distance, :max_distance, :min_weight, :max_weight, :fixed_fee, presence: true
   validates :name, uniqueness: true
 end
