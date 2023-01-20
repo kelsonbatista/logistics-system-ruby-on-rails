9142c2b842d8323a88032ed22477ab4bd550c19e
kelsonbatista <kelsonbatista@gmail.com>
2023-01-20 17:35:49 -0300

    feat: added order address associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ApplicationRecord
+  has_many :order_addresses, dependent: :destroy
+  has_many :orders, through: :order_addresses
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  has_many :order_addresses, dependent: :destroy
+  has_many :addresses, through: :order_addresses
+  has_many :products, dependent: :destroy
 end
