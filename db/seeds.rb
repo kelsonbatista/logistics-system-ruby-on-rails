6cf5e0dc15d2154bf022926552d7ccb6a74dc392
kelsonbatista <kelsonbatista@gmail.com>
2023-01-29 16:20:59 -0300

    refactor: new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,136 +1,67 @@
-########### MODES #############
-5.times do |i|
-  Mode.create!(
-    name: "Modalidade #{i + 1}",
-    min_distance: 10 * (i + 1),
-    max_distance: 100 * (i + 1),
-    min_weight: 1 * (i + 1),
-    max_weight: 100 * (i + 1),
-    fixed_fee: 50,
-    active: true
-  )
-end
+Mode.create!(name: "Light Pack", min_distance: 1, max_distance: 1000, 
+  min_weight: 1, max_weight: 10, fixed_fee: 20,  active: true)
 
-############ VEHICLES #############
-for i in 1..3 do
-  Vehicle.create!(
-    plate: "ABC-123#{i}",
-    brand: "Marca #{i}",
-    model: "Modelo #{i}",
-    category: "Categoria #{i}",
-    year: "201#{i}",
-    capacity: 100 * i,
-    status: true,
-    mode_id: 1
-  )
-end
+Mode.create!(name: "Super Pack", min_distance: 10, max_distance: 500, 
+  min_weight: 5, max_weight: 20, fixed_fee: 40, active: true)
 
-############ PRICES #############
-for i in 1..3 do
-  Price.create!(
-    min_weight: 5 * i,
-    max_weight: 10 * i,
-    price_per_km: 0.11 * i,
-    mode_id: 1
-  )
-  Price.create!(
-    min_weight: 7 * i,
-    max_weight: 12 * i,
-    price_per_km: 0.16 * i,
-    mode_id: 2
-  )
-end
+Mode.create!(name: "Mega Pack", min_distance: 10, max_distance: 300, 
+  min_weight: 10, max_weight: 30, fixed_fee: 60, active: true)
 
-############# DEADLINES #############
-for i in 1..3 do
-  Deadline.create!(
-    min_distance: 5 * i,
-    max_distance: 10 * i,
-    deadline: 15 * i,
-    mode_id: 1
-  )
-  Deadline.create!(
-    min_distance: 8 * i,
-    max_distance: 14 * i,
-    deadline: 18 * i,
-    mode_id: 2
-  )
-end
+Mode.create!(name: "Ultra Pack", min_distance: 40, max_distance: 200, 
+  min_weight: 20, max_weight: 40, fixed_fee: 80, active: true)
 
-############# USERS #############
-for i in 1..2 do
-  User.create!(
-    name: "Usuario #{i}",
-    email: "usuario#{i}@email.com",
-    password: '123456',
-    role: "user"
-  )
-  User.create!(
-    name: "Admin #{i}",
-    email: "admin#{i}@email.com",
-    password: '123456',
-    role: "admin"
-  )
-end
+Mode.create!(name: "Star Pack", min_distance: 10, max_distance: 100, 
+  min_weight: 30, max_weight: 50, fixed_fee: 100, active: true)
 
-############# ADDRESSES #############
-for i in 1..5 do
-  Address.create!(
-    person: "recipient",
-    address_one: "Av Paulista, 100#{i}",
-    address_two: "Bela Vista",
-    city: "São Paulo",
-    state: "SP",
-    zip: "10000-10#{i}"
-  )
-  Address.create!(
-    person: "sender",
-    address_one: "Av Atlântica, 200#{i}",
-    address_two: "Copacabana",
-    city: "Rio de Janeiro",
-    state: "RJ",
-    zip: "20000-20#{i}"
-  )
-end
+Vehicle.create!(plate: "ABC-1231", brand: "Volkswagem", model: "Gol 1.6", category: "Passeio", 
+      year: "2011", capacity: 100, status: true, mode_id: 1)
 
-############ ORDERS #############
-for i in 1..5 do
-  Order.create!(
-    code: "LOG700#{i}",
-    distance: 100 * i,
-    status: 0
-  )
-end
+Vehicle.create!(plate: "ABC-1232", brand: "Fiat", model: "Fiorino 1.6", category: "Carga Leve", 
+      year: "2012", capacity: 400, status: true, mode_id: 1)
 
-############ ORDER ADDRESS #############
-for i in 1..5 do
-  for j in 1..2 do
-    OrderAddress.create!(
-      order_id: i,
-      address_id: j
-    )
-  end
-end
+Vehicle.create!(plate: "ABC-1233", brand: "Ford", model: "Furgão 4.0", category: "Carga Média", 
+      year: "2013", capacity: 1000, status: true, mode_id: 1)
 
-############# PRODUCTS #############
-for i in 1..20 do
-  Product.create!(
-    code: "ABC100#{i}",
-    width: 50 * i/2,
-    height: 30 * i/2,
-    depth: 10 * i/2,
-    weight: 100 * i/2
-  )
-end
+Price.create!(min_weight: 1, max_weight: 3, price_per_km: 0.11, mode_id: 1)
+Price.create!(min_weight: 4, max_weight: 7, price_per_km: 0.22, mode_id: 1)
+Price.create!(min_weight: 8, max_weight: 10, price_per_km: 0.33, mode_id: 1)
 
-########## ORDER PRODUCT #############
-x = 0
-for i in 1..5 do
-  for j in 1..4 do
-    OrderProduct.create!(
-      order_id: i,
-      product_id: j + x
-    )
-  end
-  x = x + 4 if i < 5
-end
+Price.create!(min_weight: 5, max_weight: 10, price_per_km: 0.16, mode_id: 2)
+Price.create!(min_weight: 11, max_weight: 20, price_per_km: 0.32, mode_id: 2)
+Price.create!(min_weight: 21, max_weight: 30, price_per_km: 0.48, mode_id: 2)
+Price.create!(min_weight: 31, max_weight: 40, price_per_km: 0.64, mode_id: 2)
+Price.create!(min_weight: 41, max_weight: 50, price_per_km: 0.80, mode_id: 2)
+
+Price.create!(min_weight: 10, max_weight: 30, price_per_km: 0.20, mode_id: 3)
+Price.create!(min_weight: 31, max_weight: 60, price_per_km: 0.40, mode_id: 3)
+Price.create!(min_weight: 61, max_weight: 100, price_per_km: 0.60, mode_id: 3)
+
+Deadline.create!(min_distance: 1, max_distance: 100, deadline: 10, mode_id: 1)
+Deadline.create!(min_distance: 101, max_distance: 500, deadline: 15, mode_id: 1)
+Deadline.create!(min_distance: 501, max_distance: 1000, deadline: 20, mode_id: 1)
+
+Deadline.create!(min_distance: 10, max_distance: 100, deadline: 5, mode_id: 2)
+Deadline.create!(min_distance: 101, max_distance: 250, deadline: 7, mode_id: 2)
+Deadline.create!(min_distance: 251, max_distance: 500, deadline: 10, mode_id: 2)
+
+Deadline.create!(min_distance: 10, max_distance: 100, deadline: 3, mode_id: 3)
+Deadline.create!(min_distance: 101, max_distance: 200, deadline: 5, mode_id: 3)
+Deadline.create!(min_distance: 201, max_distance: 300, deadline: 7, mode_id: 3)
+
+Address.create!(person: "sender", address_one: "Av Atlântica, 2002", address_two: "Copacabana", 
+      city: "Rio de Janeiro", state: "RJ", zip: "20000-202")
+Address.create!(person: "recipient", address_one: "Av Paulista, 1001", address_two: "Bela Vista", 
+      city: "São Paulo", state: "SP", zip: "10000-101")
+
+@order = Order.create!(distance: 29)
+
+OrderAddress.create!(order_id: 1, address_id: 1)
+OrderAddress.create!(order_id: 1, address_id: 2)
+
+Product.create!(width: 50, height: 30, depth: 10, weight: 3)
+Product.create!(width: 70, height: 50, depth: 40, weight: 6)
+Product.create!(width: 90, height: 70, depth: 80, weight: 4)
+
+OrderProduct.create!(order_id: 1, product_id: 1)
+OrderProduct.create!(order_id: 1, product_id: 2)
+OrderProduct.create!(order_id: 1, product_id: 3)
